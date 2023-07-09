193f08aaecb88dd43b448a5baf1fe96786d2c1be
edsouz01 <x9h2q@students.cs.ubc.ca>
2023-07-10 00:18:18 +0200

    Recursive algo to solve for teeing from both sides

--- main.py
+++ main.py
@@ -6,38 +6,40 @@ from data import *
 def generate_draw(names, start_time, both_tees):
         
     random.shuffle(names)
-    draw = dict()
     
     num_teams = int(len(names) / 4)
     if len(names) % 4 != 0:
         num_teams += 1
         
     if both_tees:
-        pass
+        half_field = int(len(names) / 2)
+        first_tee_draw = generate_draw(names[:half_field], start_time, False)
+        tenth_tee_draw = generate_draw(names[half_field:], start_time, False)
+        draw = [first_tee_draw, tenth_tee_draw]
     else:
         # initializing draw dictionary
-        initialize_dict(time_str_to_int(start_time), draw, num_teams)
+        draw = initialize_dict(start_time, num_teams)
         
         # assign teams to each time
-        assign_players(names, start_time, draw)
+        assign_players(names, draw)
             
     return draw
 
-def assign_players(names, start_time, draw):
-    current_time = time_str_to_int(start_time)
+def assign_players(names, draw):
     try:
         while len(names) != 0:    
-            for team in draw.keys():
-                draw[f'{current_time}'].append(names.pop())
-                current_time += 10
-            current_time = time_str_to_int(start_time)
+            for tee_time in draw.keys():
+                draw[f'{tee_time}'].append(names.pop())
     except IndexError as e:
         print("Ran out of players")
 
-def initialize_dict(current_time, draw, num_teams):
+def initialize_dict(start_time, num_teams):
+    current_time = time_str_to_int(start_time)
+    draw = dict()
+    
     for team in range(1, num_teams + 1):
             draw[f'{current_time}'] = []
             current_time += 10
-    print(draw)
+    return draw
 
-print(generate_draw(names, "11:00", False))
+print(generate_draw(names, "11:00", True))
